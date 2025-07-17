19a9bf3e9f06329d59ee97d05cd6da234e4b58d4
Shravan Sathiyanarayanan <bl.en.u4cse23053@bl.students.amrita.edu>
2025-07-17 15:43:54 +0530

    init lab-2

---
+++ assignment2/q1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+
+df = pd.read_excel('C:\Users\shrav\Desktop\ML-sem-5\Machine-Learning-Assignments\assignment2\Lab Session Data.xlsx')
+
+A = df[['Candies (#)', 'Mangoes (Kg)', 'Milk Packets (#)']].values
+C = df['Payment (Rs)'].values
+
+dimensionality = A.shape[1]  # number of columns/features
+num_vectors   = A.shape[0]  # number of rows/data points
